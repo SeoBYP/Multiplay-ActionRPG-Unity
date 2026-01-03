c26633be5b8401510a124c7c1b8dd2e2d223067a
SeoBYP <jajazxcv@naver.com>
2026-01-03 16:44:27 +0900

    Server CSharp으로 변경

--- Server/Client/ChatPacketExport.cpp
+++
@@ -1,4 +0,0 @@
-#include "ChatPacket.h"
-#include <boost/serialization/export.hpp>
-
-BOOST_CLASS_EXPORT_GUID(ChatPacket, "ChatPacket")  // 실행파일(Client)에만 포함
--- Server/Common/C_SetNicknamePacket.cpp
+++
@@ -1,2 +0,0 @@
-#include "pch.h"
-#include "C_SetNicknamePacket.h"
